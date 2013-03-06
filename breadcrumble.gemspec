dad557b0f47b35a00aa633c7b0822a02f62e28f6
ma2ge <takayuki.1229@gmail.com>
2013-03-06 23:30:44 +0900

    Bump to Rails 4.0.0.beta1

--- breadcrumble.gemspec
+++ breadcrumble.gemspec
@@ -15,7 +15,7 @@ Gem::Specification.new do |gem|
   gem.require_paths = ["lib"]
   gem.version       = Breadcrumble::VERSION
 
-  gem.add_dependency "rails", ['>= 3.0']
+  gem.add_dependency "rails", ['>= 4.0.0.beta1']
   gem.add_development_dependency "sqlite3"
   gem.add_development_dependency "rspec-rails"
 end
--- spec/dummy/config/initializers/secret_token.rb
+++ spec/dummy/config/initializers/secret_token.rb
@@ -2,6 +2,11 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '950c9b0bb4babc88ef4686141d4cb64ac5ea5e0f28f6ddfb84907801d3bb3bb05151d1537f393294fa93174f4ebebdb855140359923833862f9468fef2e31105'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = 'ae3cad0b2a7fe9cf1620b58b3629d5238578e36ba303b9821fb22ac023c9dc1b07ef16f27b74e96c4294f1e02c5bfc83f2dc89c02c9fc24d1b3974f6d7c14701'
