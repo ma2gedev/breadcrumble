d73530511bd2df0a7b10eedb81d7d8eb6b91ee6c
Takayuki Matsubara <takayuki.1229@gmail.com>
2013-06-19 23:40:49 +0900

    set up coveralls

--- breadcrumble.gemspec
+++ breadcrumble.gemspec
@@ -18,4 +18,5 @@ Gem::Specification.new do |gem|
   gem.add_dependency "rails", ['>= 4.0.0.rc1']
   gem.add_development_dependency "sqlite3"
   gem.add_development_dependency "rspec-rails"
+  gem.add_development_dependency "coveralls"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,9 @@ require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
+require 'coveralls'
+Coveralls.wear!
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
