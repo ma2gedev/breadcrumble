4c9489f3a7b271480b9de60091034fe88e5391c3
Takayuki Matsubara <takayuki.1229@gmail.com>
2013-07-15 00:42:40 +0900

    added render_breadcrumb_trails specs

--- spec/helpers/action_view_spec.rb
+++ spec/helpers/action_view_spec.rb
@@ -14,4 +14,27 @@ describe 'Breadcrumble::ActionView' do
     it { should match('test_name') }
     it { should match('test_url') }
   end
+  context '#render_breadcrumb_trails' do
+    before do
+      helper.stub!(:breadcrumb_trails).and_return([
+        [
+          { name: 'trail1_level1', url: '/trail1_level1_url' },
+          { name: 'trail1_level2', url: '/trail1_level2_url' }
+        ],
+        [
+          { name: 'trail2_level1', url: '/trail2_level1_url' },
+          { name: 'trail2_level2', url: '/trail2_level2_url' }
+        ]
+      ])
+    end
+    subject { helper.render_breadcrumb_trails }
+    it { should match('trail1_level1') }
+    it { should match('trail1_level1_url') }
+    it { should match('trail1_level2') }
+    it { should match('trail1_level2_url') }
+    it { should match('trail2_level1') }
+    it { should match('trail2_level1_url') }
+    it { should match('trail2_level2') }
+    it { should match('trail2_level2_url') }
+  end
 end
