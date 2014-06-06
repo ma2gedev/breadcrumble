c93a0d93f2082ea34bc9016b9458d6ba887dd070
Takayuki Matsubara <takayuki.1229@gmail.com>
2014-06-06 23:55:58 +0900

    remove `require 'rspec/autorun'`
    - deprecation warning message is the following
    Requiring `rspec/autorun` when running RSpec via the `rspec` command is deprecated.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 
 require 'coveralls'
 Coveralls.wear!
