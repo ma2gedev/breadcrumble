6034834fe577e077b5c8898c1002d0e502e6b770
Takayuki Matsubara <takayuki.1229@gmail.com>
2014-06-07 01:07:53 +0900

    supports theme function
    fixes #1

--- lib/breadcrumble/action_view.rb
+++ lib/breadcrumble/action_view.rb
@@ -1,13 +1,20 @@
 module Breadcrumble
   module ActionView
     # Render breadcrumbs.
-    def render_breadcrumbs(options = nil)
-      render partial: 'breadcrumble/breadcrumb', locals: options
+    def render_breadcrumbs(options = {})
+      _render_breadcrumb('breadcrumb', options)
     end
 
     # Render breadcrumb trails.
-    def render_breadcrumb_trails(options = nil)
-      render partial: 'breadcrumble/breadcrumb_trails', locals: options
+    def render_breadcrumb_trails(options = {})
+      _render_breadcrumb('breadcrumb_trails', options)
+    end
+
+    private
+
+    def _render_breadcrumb(path, options)
+      theme = options[:theme] ? "#{options[:theme]}/" : ''
+      render partial: "breadcrumble/#{theme}#{path}", locals: options
     end
   end
 end
--- spec/helpers/action_view_spec.rb
+++ spec/helpers/action_view_spec.rb
@@ -37,4 +37,30 @@ describe 'Breadcrumble::ActionView' do
     it { is_expected.to match('trail2_level2') }
     it { is_expected.to match('trail2_level2_url') }
   end
+  context '#render_breadcrumbs with theme' do
+    before do
+      allow(helper).to receive(:breadcrumbs).and_return([
+        { name: 'root_name', url: '/root_url' },
+        { name: 'test_name', url: '/test_url' }
+      ])
+    end
+    subject { helper.render_breadcrumbs(theme: 'test') }
+    it { is_expected.to match('test breadcrumb theme') }
+  end
+  context '#render_breadcrumb_trails with theme' do
+    before do
+      allow(helper).to receive(:breadcrumb_trails).and_return([
+        [
+          { name: 'trail1_level1', url: '/trail1_level1_url' },
+          { name: 'trail1_level2', url: '/trail1_level2_url' }
+        ],
+        [
+          { name: 'trail2_level1', url: '/trail2_level1_url' },
+          { name: 'trail2_level2', url: '/trail2_level2_url' }
+        ]
+      ])
+    end
+    subject { helper.render_breadcrumb_trails(theme: 'test') }
+    it { is_expected.to match('test breadcrumb_trails theme') }
+  end
 end
