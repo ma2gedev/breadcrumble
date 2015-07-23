c2b5b516f45a07a901f24ff1cb272d3a5d08f5fe
Takayuki Matsubara <takayuki.1229@gmail.com>
2015-07-24 00:06:26 +0900

    change default template markup with schema.org syntax

--- spec/helpers/action_view_spec.rb
+++ spec/helpers/action_view_spec.rb
@@ -8,12 +8,22 @@ describe 'Breadcrumble::ActionView' do
         { name: 'test_name', url: '/test_url' }
       ]}
     end
-    subject { helper.render_breadcrumbs(theme: 'data_vocabulary') }
-    it { is_expected.to match('root_name') }
-    it { is_expected.to match('root_url') }
-    it { is_expected.to match('test_name') }
-    it { is_expected.to match('test_url') }
-    it { is_expected.to match(' itemscope ') }
+    context 'schema.org' do
+      subject { helper.render_breadcrumbs }
+      it { is_expected.to match('root_name') }
+      it { is_expected.to match('root_url') }
+      it { is_expected.to match('test_name') }
+      it { is_expected.to match('test_url') }
+      it { is_expected.to match(' itemscope ') }
+    end
+    context 'data-vocabulary.org' do
+      subject { helper.render_breadcrumbs(theme: 'data_vocabulary') }
+      it { is_expected.to match('root_name') }
+      it { is_expected.to match('root_url') }
+      it { is_expected.to match('test_name') }
+      it { is_expected.to match('test_url') }
+      it { is_expected.to match(' itemscope ') }
+    end
   end
   context '#render_breadcrumb_trails' do
     before do
@@ -28,16 +38,31 @@ describe 'Breadcrumble::ActionView' do
         ]
       ]}
     end
-    subject { helper.render_breadcrumb_trails(theme: 'data_vocabulary') }
-    it { is_expected.to match('trail1_level1') }
-    it { is_expected.to match('trail1_level1_url') }
-    it { is_expected.to match('trail1_level2') }
-    it { is_expected.to match('trail1_level2_url') }
-    it { is_expected.to match('trail2_level1') }
-    it { is_expected.to match('trail2_level1_url') }
-    it { is_expected.to match('trail2_level2') }
-    it { is_expected.to match('trail2_level2_url') }
-    it { is_expected.to match('itemprop="child" itemscope itemtype="http://data-vocabulary.org/Breadcrumb"') }
+    context 'schema.org' do
+      subject { helper.render_breadcrumb_trails }
+      it { is_expected.to match('trail1_level1') }
+      it { is_expected.to match('trail1_level1_url') }
+      it { is_expected.to match('trail1_level2') }
+      it { is_expected.to match('trail1_level2_url') }
+      it { is_expected.to match('trail2_level1') }
+      it { is_expected.to match('trail2_level1_url') }
+      it { is_expected.to match('trail2_level2') }
+      it { is_expected.to match('trail2_level2_url') }
+      it { is_expected.to match('itemscope itemtype="http://schema.org/BreadcrumbList"') }
+      it { is_expected.to match('itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"') }
+    end
+    context 'data-vocabulary.org' do
+      subject { helper.render_breadcrumb_trails(theme: 'data_vocabulary') }
+      it { is_expected.to match('trail1_level1') }
+      it { is_expected.to match('trail1_level1_url') }
+      it { is_expected.to match('trail1_level2') }
+      it { is_expected.to match('trail1_level2_url') }
+      it { is_expected.to match('trail2_level1') }
+      it { is_expected.to match('trail2_level1_url') }
+      it { is_expected.to match('trail2_level2') }
+      it { is_expected.to match('trail2_level2_url') }
+      it { is_expected.to match('itemprop="child" itemscope itemtype="http://data-vocabulary.org/Breadcrumb"') }
+    end
   end
   context '#render_breadcrumbs with theme' do
     before do
