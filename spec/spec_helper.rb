3bc2be7bdc84a582a84d58222e7ba046b6ef538f
Takayuki Matsubara <takayuki.1229@gmail.com>
2015-09-19 23:41:48 +0900

    Rails edge tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,3 +22,11 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.mock_with :rr
 end
+
+if defined?(Rails::Controller::Testing)
+  RSpec.configure do |config|
+    config.include Rails::Controller::Testing::TestProcess
+    config.include Rails::Controller::Testing::TemplateAssertions
+    config.include Rails::Controller::Testing::Integration
+  end
+end
